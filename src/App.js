0c691c4cf1715bc063f254a17662ea7904428889
Renuka Choudhary <renurunrun@gmail.com>
2022-11-03 20:43:02 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <br/>
+      <h1>The Evolution of <br/> Arbnb's Frontend</h1>
+      
+      <p>@Spikebrehm</p>
+      <br/>
+      <img className="logo-img" src ="airbnb1.png"/>
+      
+      <p>airbnb</p>
+      <h4>Created with ❤️. Happy Coding..</h4>
+      <h5>Renuka Choudhary.</h5>
+
     </div>
   );
 }
